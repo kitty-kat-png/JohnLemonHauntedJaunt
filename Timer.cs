696f83b77d2faa738d1369374214048b8924de46
ciera628 <143846698+ciera628@users.noreply.github.com>
2024-12-04 19:15:18 -0500

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float TimeLeft;
+    public bool TimerOn = false;
+
+    public TextMeshProUGUI timerText;
+
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    void Update()
+    {
+        if(TimerOn)
+        {
+            if(TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is UP!");
+                TimeLeft = 0;
+                TimerOn = false;
+
+            }
+        }
+    }
+
+
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
